86591deaa54f36813388ada4aba9bed4cafae167
vk-bhanu <146329087+vk-bhanu@users.noreply.github.com>
2024-02-25 21:12:11 +0530

    fixed some more errors

--- src/pages/tasks/list.tsx
+++ src/pages/tasks/list.tsx
@@ -1,17 +1,20 @@
 import { KanbanColumnSkeleton, ProjectCardSkeleton } from '@/components'
 import { KanbanAddCardButton } from '@/components/tasks/kanban/add-card-button'
 import { KanbanBoardContainer, KanbanBoard } from '@/components/tasks/kanban/board'
-import {ProjectCard, ProjectCardMemo } from '@/components/tasks/kanban/card'
+import {ProjectCardMemo } from '@/components/tasks/kanban/card'
 import { KanbanColumn } from '@/components/tasks/kanban/column'
 import KanbanItem from '@/components/tasks/kanban/item'
 import { UPDATE_TASK_STAGE_MUTATION } from '@/graphql/mutations'
 import { TASKS_QUERY, TASK_STAGES_QUERY } from '@/graphql/queries'
-import { TaskStage } from '@/graphql/schema.types'
-import { TasksQuery } from '@/graphql/types'
+// import { TaskStage } from '@/graphql/schema.types'
+import { TaskStagesQuery, TasksQuery } from '@/graphql/types'
 import { DragEndEvent } from '@dnd-kit/core'
 import { useList, useNavigation, useUpdate } from '@refinedev/core'
 import { GetFieldsFromList } from '@refinedev/nestjs-query'
 import React from 'react'
+
+type Task = GetFieldsFromList<TasksQuery>
+type TaskStage = GetFieldsFromList<TaskStagesQuery>
 
 const List = ({ children }: React.PropsWithChildren) => {
   const { replace } = useNavigation()
@@ -66,6 +69,10 @@ const List = ({ children }: React.PropsWithChildren) => {
 
     const unassignedStage = tasks.data.filter((task) => task.stageId === null)
 
+    const grouped: TaskStage[] = stages.data.map((stage) => ({
+      ...stage,
+      tasks: tasks.data.filter((task) => task.stageId?.toString() === stage.id )
+    }))
 
     return {
       unassignedStage,
